c6b3b11f5c885301dc8bb6ad29d7b796d39251f5
shinchan2007 <kishorsuresh210@gmail.com>
2025-02-12 13:16:58 +0530

    /

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import Login from './Login'
-import Form from './Form'
+import Login from './Login.jsx'
+import Form from './Form.jsx'
 
 const router = createBrowserRouter([
   {
